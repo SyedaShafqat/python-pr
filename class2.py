bca13c62b4b870939d1bcaf2098471c8aa0deb88
SyedaShafqat <124997106+SyedaShafqat@users.noreply.github.com>
2024-03-05 16:27:25 +0500

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,24 @@
+print("names of list")
+names = ["anisa", "sundas", "maria", "sidra"]
+print(names)
+print('\n')
+print("replace name on index 1")
+names = ["anisa", "sundas", "maria", "sidra"]
+names[1] = 'shafqat'
+print(names)
+print('\n')
+print("append a new name")
+names = ["anisa", "sundas", "maria", "sidra"]
+names.append("sara")
+print(names)
+print('\n')
+print("insert name on index 2")
+names = ["anisa", "sundas", "maria", "sidra"]
+names.insert(1, "rida")
+print(names)
+print('\n')
+print("extend list 1")
+names = ["anisa", "sundas", "maria", "sidra"]
+numbers = [22, 23, 24, 25]
+names.extend(numbers)
+print(names)
